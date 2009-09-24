2e914bf61c4611531e2ebd7c468702be367ebc6b
Chandan Uddaraju <chandanu@quicinc.com>
2009-09-23 23:18:57 -0700

    vendor/qcom/msm7x30: Enable keypad driver support in bootloader for Fastboot on 7x30
    Enable keypad driver support in bootloader for Fastboot on 7x30.
    
    CRs-fixed: 211026

--- boot/keypad.c
+++ boot/keypad.c
@@ -1,28 +1,28 @@
-/* Copyright 2007, Google Inc. */
+/*
+ * Copyright 2007, Google Inc.
+ * Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+*/
 
 #include <boot/boot.h>
 #include <boot/gpio_keypad.h>
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
-static unsigned int halibut_row_gpios[] = { 31, 32, 33, 34, 35, 41 };
-static unsigned int halibut_col_gpios[] = { 36, 37, 38, 39, 40 };
+static unsigned char qwerty_keys_old[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+					   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+static unsigned char qwerty_keys_new[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+					   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-static unsigned int halibut_key_map[] = {
-	[11] = BOOT_KEY_CONTINUE_BOOT,      /* FA on SURF, B on FFA */
-	[23] = BOOT_KEY_STOP_BOOT,          /* FB on SURF */
-    [27] = BOOT_KEY_STOP_BOOT,          /* 2 on FFA */
+static unsigned int qwerty_keypad_map[] = {
+	[34] = BOOT_KEY_STOP_BOOT,          /* -L on SURF & FFA */
 };
 
-static gpio_keypad_info halibut_keypad = {
-    .output_gpios = halibut_row_gpios,
-    .input_gpios = halibut_col_gpios,
-    .noutputs = ARRAY_SIZE(halibut_row_gpios),
-    .ninputs = ARRAY_SIZE(halibut_col_gpios),
-    .key_map = halibut_key_map,
-    .settle_time = 5000,
-    .polarity = 0,
-    .drive_inactive_outputs = 1
+static qwerty_keypad_info qwerty_keypad = {
+    .old_keys = qwerty_keys_old,
+    .rec_keys = qwerty_keys_new,
+    .rows = ARRAY_SIZE(qwerty_keys_new),
+    .columns = sizeof(unsigned char) * 8,
+    .key_map = qwerty_keypad_map,
 };
 
 static void keypad_poll()
@@ -30,13 +30,13 @@ static void keypad_poll()
     static int skip = 0;
     skip++;
     if(skip > 10) {
-        gpio_keypad_scan_keys(&halibut_keypad);
+	scan_qwerty_keypad(&qwerty_keypad);
         skip = 0;
     }
 }
 
 void keypad_init(void)
 {
-    gpio_keypad_init(&halibut_keypad);
+    ssbi_keypad_init();
     boot_register_poll_func(keypad_poll);
 }
