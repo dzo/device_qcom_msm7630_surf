cf1d8b2ad309414901cf7e42b9013224b3a90807
David Ng <dave@codeaurora.org>
2010-07-08 16:01:10 -0700

    msm7x30: Use NEON target variant and set v7a EABI support
    Change-Id: If6ef19b39c02def2c6b83f80b0b95f96a4b30e0d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,10 +47,9 @@ TARGET_NO_BOOTLOADER := false
 TARGET_NO_KERNEL := false
 TARGET_NO_RADIOIMAGE := true
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_CPU_ABI := armeabi
-TARGET_ARCH_VARIANT := armv7-a
+TARGET_CPU_ABI  := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_HARDWARE_3D := false
