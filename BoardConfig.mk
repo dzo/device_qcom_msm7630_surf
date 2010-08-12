5da7b523fede5951210c47aa2e7823378b1a7738
Susheel Yadagiri <syadagir@codeaurora.org>
2010-08-12 16:48:34 -0700

    Define BOARD_USES_QCNE
    Change-Id: I913c2b875913eab2f426e934b54a846d635ea64c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ ifeq ($(QC_PROP),true)
     HAVE_ADRENO200_FIRMWARE := true
     BOARD_USES_QCOM_AUDIO_V2 := true
     BOARD_USE_QCOM_TESTONLY := true
+    BOARD_USES_QCNE := true
 
     ifneq ($(BUILD_TINY_ANDROID), true)
     BOARD_GPS_LIBRARIES := libloc
