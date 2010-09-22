23394b7f2df74e69880cc8abd9bf0d46cb0ef1c0
Kinson Chik <kchik@codeaurora.org>
2010-09-22 16:38:59 -0700

    qcom/msm7630_surf: Push xml files to /etc/permissions
    These files are needed in froyo to have framework report
    the correct SystemFeatures. Newest CTS tests check for this
    and are failing due to the files not being pushed.
    
    Change-Id: I68cabefca27cda5a1f964c89c840856236676c87

--- msm7630_surf.mk
+++ msm7630_surf.mk
@@ -3,6 +3,19 @@ PRODUCT_PACKAGES := \
     VoiceDialer \
     FM
 
+PRODUCT_COPY_FILES := \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/base/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+
+PRODUCT_COPY_FILES += \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 $(call inherit-product, build/target/product/generic.mk)
 
