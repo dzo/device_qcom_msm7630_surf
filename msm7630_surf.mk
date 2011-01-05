1596748e21a7fb4185595334e207ab98a748409f
Naseer Ahmed <naseer@codeaurora.org>
2011-01-05 12:31:22 +0530

    msm7630_surf: Use common include
    Use the common.mk in device/qcom/common
    instead of defining everything here.
    Any property that needs to change can
    be overridden here.
    
    Change-Id: Ia7352c3cdec41fa76046c19ccf19ce86649e0b7e

--- msm7630_surf.mk
+++ msm7630_surf.mk
@@ -1,30 +1,4 @@
-PRODUCT_PACKAGES := \
-    IM \
-    VoiceDialer \
-    FM
+$(call inherit-product, device/qcom/common/common.mk)
 
-PRODUCT_COPY_FILES := \
-    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    #frameworks/base/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-
-PRODUCT_COPY_FILES += \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
-
-$(call inherit-product, build/target/product/generic.mk)
-
-#Enabling Ring Tones
-include frameworks/base/data/sounds/OriginalAudio.mk
-
-# Overrides
-PRODUCT_BRAND := qcom
 PRODUCT_NAME := msm7630_surf
 PRODUCT_DEVICE := msm7630_surf
-PRODUCT_LOCALES := hdpi
