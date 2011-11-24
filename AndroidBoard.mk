99fa3cfcf890dbd9e883fdb52d78a0addf1cd1e4
Venkat Ram Prakash Kotni <vkotni@codeaurora.org>
2011-11-24 18:50:40 +0530

    device/qcom/msm7630_surf: Set proper lib path.
    Detect type of 7630 modem from sys file system. If it is msm7630 4.0 modem,
    set rild.libpath to RPC RIL. Otherwise set it to QMI RIL
    
    Change-Id: I23567c59255d5252fca4b32e1927a2a14cbc8214
    (cherry picked from commit c7582294a0ce1941fa00944e4ca3d227826972f6)

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -96,6 +96,14 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.ril.path.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant.conf
