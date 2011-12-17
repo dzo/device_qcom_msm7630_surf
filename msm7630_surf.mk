c36d74efda12423be6a2488b8abd1d5f8f959283
Ajay Dudani <adudani@codeaurora.org>
2011-12-17 12:03:47 -0800

    msm7630: Drop obsolete inherit-if-exists line and property overrides
    Change-Id: Ib928c804c69cccc4abaab2fea231a3ec2e1a0eac

--- msm7630_surf.mk
+++ msm7630_surf.mk
@@ -1,8 +1,4 @@
 $(call inherit-product, device/qcom/common/common.mk)
-$(call inherit-product-if-exists, vendor/qcom/proprietary/tools/google/gingerbread/products/mini-gms.mk)
 
 PRODUCT_NAME := msm7630_surf
 PRODUCT_DEVICE := msm7630_surf
-
-PRODUCT_PROPERTY_OVERRIDES += \
- ro.com.google.clientidbase=android-qualcomm
