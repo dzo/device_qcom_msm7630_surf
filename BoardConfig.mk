7ad7244a8a7c05a5b465011a9e75da4ef58aa494
Neti Ravi Kumar <ravineti@codeaurora.org>
2012-01-16 11:45:56 +0530

    Enable composition bypass for UI layers on 8x55
    Enable composition bypass on 8x55. Maximum number of bypass layers
    is decided by hardware composer.
    
    Change-Id: Iffcb76946f6c366257c8e0d8b85c5e0ec3615691

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,7 @@ ifeq ($(QC_PROP),true)
     TARGET_USES_SF_BYPASS := false
     WEBCORE_INPAGE_VIDEO := true
     USE_OPENGL_RENDERER := true
+    TARGET_HAVE_BYPASS := true
 
     ifneq ($(BUILD_TINY_ANDROID), true)
     BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50001
